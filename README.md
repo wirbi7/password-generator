1010bdf5edb00fe0b31a9bd3faaad7f8c770b100
Игорь Некрасов <igor.nekrasov@kub3.ru>
2015-05-17 10:50:00 +0300

    setup

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # password-generator
 Little Linux Password Generator written on Python 3 with Gtk
 ![Application screen](/img/img1.png?raw=true "Application screen")
+## Installation
+```
+sudo pip3 install password-generator-gtk
+```
--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='password-generator',
+    name='password-generator-gtk',
     version='0.1',
     url='https://github.com/wirbi7/password-generator',
     license='GNU GPL 2.0',
     author='Igor Nekrasov',
     author_email='nekrasov.i.m@gmail.com',
     description='Little Linux Password Generator written on Python 3 with Gtk',
-    packages=['passwordgenerator'],
-    package_dir={'passwordgenerator': 'passwordgenerator'},
-    package_data={'passwordgenerator': ['pasgen.glade']},
+    packages=['passwordgeneratorgtk'],
+    package_dir={'passwordgeneratorgtk': 'passwordgeneratorgtk'},
+    package_data={'passwordgeneratorgtk': ['pasgen.glade']},
     scripts=['pasgen'],
     classifiers=[
         'Development Status :: 4 - Beta',
